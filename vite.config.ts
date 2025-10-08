7a1ff9401b7048ca381f66a2b097e8648e36a41d
Felipe <fehscudiero@live.com>
2025-10-08 14:57:25 -0300

    otimização

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react"; // troquei o plugin
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    host: "::",
-    port: 8080,
+    host: "localhost",
+    port: 3000,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    target: "es2015",
+    minify: "esbuild",
+    sourcemap: false,
+    assetsInlineLimit: 8192,
+  },
+});
