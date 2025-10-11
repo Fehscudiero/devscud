3e13d9d32b71d7968f6a03e35ea8c940720a9a9b
Felipe <fehscudiero@live.com>
2025-10-11 19:28:51 -0300

    Arrumando espaçamento quando é clikado no menu

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -68,7 +68,7 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact" className="py-20 pb-8">
+    <section id="contact" className="py-6 pb-1">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <div className="text-center mb-16 space-y-4">
